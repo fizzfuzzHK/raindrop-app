c77928743f49058743b64bbb337032e1578c1aae
fizzfuzzhk <fizzfuzzhk@gmail.com>
2019-08-12 20:44:48 +0900

    first

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,9 @@
-
-
-
-
-var isRainMade = false;
-var rains = []
-var a = 0;
-
+//making sound by Tone.js
 var delay = new Tone.FeedbackDelay(0.6,0.5);
+delay.wet.value = 0.4;
+
 var limiter = new Tone.Limiter(-6);
 
-delay.wet.value = 0.4;
 let masterComp = new Tone.Compressor(-30,3).toMaster();
 
 var option = {
@@ -23,9 +17,15 @@ var option = {
  release: 4.5
  }
 };
+
+//create Synth
 const synth = new Tone.Synth(option).chain(delay, limiter, Tone.Master);
 
+//p5.js
+var isRainMade = false;
+var rains = []
 
+//Rain class
 class Rain{
   constructor(x,y){
   this.x = x;
@@ -36,7 +36,6 @@ class Rain{
   draw(x,y){
     stroke(255);
     ellipse(this.x,this.y,10,10);
-
   }
 
   fall(){
@@ -44,15 +43,16 @@ class Rain{
       this.y += this.gravity;
     }
   }
-
 }
 
+//setup
 function setup(){
   createCanvas(800,300);
   frameRate(100);
 
 }
 
+//create instance when mouseClicked
 function mouseClicked(){
   var g = new Rain(mouseX,mouseY);
   rains.push(g);
@@ -60,50 +60,49 @@ function mouseClicked(){
   return false;
   }
 
-
+//draw
 function draw(){
   background(27,15,66);
 
-   let d = color(221,253,254);
+  let d = color(221,253,254);
   fill(d);
 
   if(isRainMade == true){
-  for(let i = rains.length -1 ; i >= 0 ;i--){
-  rains[i].draw();
-  rains[i].fall();
+    for(let i = rains.length -1 ; i >= 0 ;i--){
+      rains[i].draw();
+      rains[i].fall();
 
-  if(rains[i].y > 300){
-    var c;
-    if(rains[i].x > 0 && rains[i].x < 100){
-      c = "C5";
+      if(rains[i].y > 300){
+        var c;
+        if(rains[i].x > 0 && rains[i].x < 100){
+          c = "C5";
+        }
+        else if(rains[i].x > 100 && rains[i].x < 200){
+          c = "D5";
+        }
+        else if(rains[i].x > 200 && rains[i].x < 300){
+          c = "G5";
+        }
+        else if(rains[i].x > 300 && rains[i].x < 400){
+          c = "A5";
+        }
+        else if(rains[i].x > 400 && rains[i].x < 500){
+          c = "B5";
+        }
+        else if(rains[i].x > 500 && rains[i].x < 600){
+          c = "C6";
+        }
+        else if(rains[i].x > 600 && rains[i].x < 700){
+          c = "D6";
+        }
+        else if(rains[i].x > 700 && rains[i].x < 800){
+        c = "E6";
+        }
+        synth.triggerAttackRelease(c, "8n");
+      }
+      if(rains[i].y > 303){
+        rains.splice(i,1);
+      }
     }
-    else if(rains[i].x > 100 && rains[i].x < 200){
-      c = "D5";
-    }
-    else if(rains[i].x > 200 && rains[i].x < 300){
-      c = "G5";
-    }
-    else if(rains[i].x > 300 && rains[i].x < 400){
-      c = "A5";
-    }
-    else if(rains[i].x > 400 && rains[i].x < 500){
-      c = "B5";
-    }
-    else if(rains[i].x > 500 && rains[i].x < 600){
-      c = "C6";
-    }
-    else if(rains[i].x > 600 && rains[i].x < 700){
-      c = "D6";
-    }
-    else if(rains[i].x > 700 && rains[i].x < 800){
-      c = "E6";
-    }
-    synth.triggerAttackRelease(c, "8n");
-     }
-     if(rains[i].y > 303){
-       rains.splice(i,1);
-     }
-  }
-
   }
 }
